f6f9f13bc29b6da4b6c0b53a981d7b624e4322df
HATBE <admin@aarongensetter.ch>
2022-01-17 11:15:28 +0100

    refactor

--- Random_Clicker_GAME_1/script.js
+++ Random_Clicker_GAME_1/script.js
@@ -1,36 +1,105 @@
-// My First JS Game!!
+const counterEl = document.getElementById('pointsCounter');
+const clickerButtonEL = document.getElementById('clickerButton');
+const msgWrapperEl = document.getElementById('msgWrapper');
+const shopEl = document.getElementById('shop');
+const saveBtn = document.querySelector('.saveBtn');
 
-const clickerEl = document.getElementById('clicker');
-const counterEl = document.getElementById('counter');
-const msgWrapper = document.getElementById('msgWrapper');
-
-const shopItems = document.querySelectorAll('.shopItem');
-
-const autoclickerCountEl = document.getElementById('autoclickerItemCount');
-const multiplayerCountEl = document.getElementById('multiplayerItemCount');
-const autoclickerPriceEl = document.getElementById('autoclickerItemPrice');
-const multiplayerPriceEl = document.getElementById('multiplayerItemPrice');
-
-
-let money;
-let autoclickerCount;
-let currentMultiply;
-
-const cost = {
-    'autoclicker': {
-        'cost': 83
+let shopItems = [
+    {
+        name: 'CPSBoost',
+        cost: 100,
+        maxCount: 30,
+        amount: 0,
+        onClick: function() {
+            
+        }
     },
-    'multiplayer': {
-        'cost': 167
+    {
+        name: 'Autoclicker',
+        cost: 130,
+        maxCount: 10,
+        amount: 0,
+        onClick: function() {
+            
+        }
     }
-}
+]
+
+let points;
 
 function init() {
-    currentMultiply = Number(getStandard('currentMultiply', 1)); 
-    money = Number(getStandard('money', 0));
-    autoclickerCount = Number(getStandard('autoclickerCount', 1));
+    points = Number(getValueStandard('points', 0));
+    shopItems.forEach((item) => {
+        item.amount = Number(getValueStandard(`shopItem-${item.name}`, 0));
+    });
 
+    createShop();
     updateUi();
+}
+
+function createNotification(text, cclass = 'blue') {
+    const msgEl = document.createElement('div');
+    msgEl.classList.add('msg', cclass);
+    msgEl.textContent = text;
+
+    msgWrapperEl.appendChild(msgEl);
+    setTimeout(() => msgEl.remove(), 3000);
+}
+
+function addPoints(amt) {
+    points += amt;
+}
+
+function removePoints(amt) {
+    points -= amt;
+}
+
+function clicked(e) {
+    e.preventDefault();
+
+    let x = e.clientX - clickerButtonEL.offsetLeft;
+    let y = e.clientY - clickerButtonEL.offsetTop;
+    
+    let amt = shopItems[0].amount == 0 ? 1 : (shopItems[0].amount + 1) ** 2;
+
+    spawnFloatingNumber(x, y, amt);
+
+    addPoints(amt);
+    updateUi();
+}
+
+function spawnFloatingNumber(x, y, amt) {
+    let floatingNumber = document.createElement('span');
+    floatingNumber.classList.add('floatingNumber');
+    floatingNumber.textContent = `+${numberWithCommas(amt)}`;
+
+    floatingNumber.style.top = `${y}px`;
+    floatingNumber.style.left = `${x}px`;
+
+    clickerButtonEL.appendChild(floatingNumber);
+
+    setTimeout(() => floatingNumber.remove(), 1950);
+}
+
+function save() {
+    if(typeof points == 'number' && points > 0) {
+        setCookie('points', points, 365);
+    }
+    shopItems.forEach((item) => {
+        if(typeof item.amount == 'number' && item.amount > 0) {
+            setCookie(`shopItem-${item.name}`, item.amount, 365);
+        }
+    });
+
+    createNotification('saved!');
+}
+
+function getValueStandard(value, standard) {
+    value = getCookie(value);
+    if(value !== '') {
+        return value;
+    }
+    return standard;
 }
 
 function setCookie(cname, cvalue, exdays) {
@@ -53,113 +122,114 @@ function getCookie(cname) {
         return c.substring(name.length, c.length);
       }
     }
-    return "";
+    return '';
 }
 
-function addMoney(num) {
-    money = money + Number(num);
+function numberWithCommas(num) {
+    if(num < 1_000_000) {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'");
+    }
+
+    const lookup = [
+        { value: 1e6, symbol: " Million" },
+        { value: 1e9, symbol: " Billion" },
+        { value: 1e12, symbol: " Trillion" },
+        { value: 1e15, symbol: " Quadrillion" },
+        { value: 1e18, symbol: " Quintillion" }
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    let item = lookup.slice().reverse().find(function(item) {
+    return num >= item.value;
+    });
+    return item ? (num / item.value).toFixed(3).replace(rx, "$1") + item.symbol : "0";
 }
 
-function removeMoney(num) {
-    money -= Number(num);
-}
-
-function createMsg(text) {
-    const msg = document.createElement('div');
-    msg.classList.add('msg');
-    msg.textContent = text;
-
-    msgWrapper.appendChild(msg);
-
-    setTimeout(() => {
-        msg.remove();
-    }, 3000)
-}
-
-function clicked() {
-    addMoney(1 * currentMultiply);
-    updateUi();
-}
-
-function getStandard(name, std) {
-    let value = getCookie(name);
-    if (value != "") {
-        return value;
+function buyItem(cost) {
+    if(cost > points) {
+        createNotification(`Not enough Points! (${points} of ${cost})`, 'red');
+        return false;
     } else {
-        return std;
-    }
-}
+        removePoints(cost);
 
-function save() {
-    if(typeof money == 'number') {
-        if(money > 0) {
-            setCookie('money', money, 365);
-        }
-    }
-    if(typeof autoclickerCount == 'number') {
-        if(autoclickerCount > 0) {
-            setCookie('autoclickerCount', autoclickerCount, 365);
-        }
-    }
-    if(typeof currentMultiply == 'number') {
-        if(currentMultiply > 0) {
-            setCookie('currentMultiply', currentMultiply, 365);
-        }
-    }
-
-    createMsg(`Saved!`);
-    updateUi();
-}
-
-function autoClicker() {
-    if(autoclickerCount > 0) {
-        addMoney(1 * autoclickerCount * currentMultiply);
+        createNotification('Purchased Item', 'blue');
+        return true;
     }
 }
 
 function updateUi() {
-    counterEl.textContent = money;
-    multiplayerCountEl.textContent = currentMultiply;
-    autoclickerCountEl.textContent = autoclickerCount;
+    counterEl.textContent = numberWithCommas(points);
+    shopItems.forEach((item) => {
+        cost = item.amount == 0 ? item.cost : Math.round(item.cost * item.amount * 22);
 
-    autoclickerPriceEl.textContent = Math.round(cost.autoclicker.cost * (autoclickerCount * 1.2));
-    multiplayerPriceEl.textContent = Math.round(cost.multiplayer.cost * (currentMultiply * 1.2));
+        let costEl = document.getElementById(`shopItem-${item.name}-cost`);
+        let amtEl = document.getElementById(`shopItem-${item.name}-amt`);
+
+        costEl.textContent = `Cost: ${cost}`;
+        amtEl.textContent = `Amount: ${item.amount}`;
+    });
 }
 
-function buyItem(item) {
-    if(item == 'autoclickerItem') {
-        if(money >= Math.round(cost.autoclicker.cost * (autoclickerCount * 1.2))) {
-            removeMoney(Math.round(cost.autoclicker.cost * (autoclickerCount * 1.2)));
-            autoclickerCount++;
-            createMsg('Buy');
-         } else {
-             createMsg('Not enough Money');
-         }
-    } else if(item == 'multiplayerItem') {
-        if(money >= Math.round(cost.multiplayer.cost * (currentMultiply * 1.2))) {
-            removeMoney(Math.round(cost.multiplayer.cost * (currentMultiply * 1.2)));
-            currentMultiply++;
-            createMsg('Buy');
-        } else {
-            createMsg('Not enough Money');
-        }
+function createShop() {
+    shopItems.forEach((item, idx) => {
+        let cost = item.amount == 0 ? item.cost : Math.round(item.cost * (item.amount * 2) * 22);
+        let shopItem = document.createElement('button');
+        shopItem.id = `shopItem-${item.name}`;
+        shopItem.classList.add('shopItem');
+        
+        let nameEl = document.createElement('div');     
+        nameEl.id = `shopItem-${item.name}-name`;                           
+        nameEl.textContent = item.name;
+        nameEl.style.fontWeight = 'bold';
+        shopItem.appendChild(nameEl);
+
+        let costEl = document.createElement('div');
+        costEl.id = `shopItem-${item.name}-cost`;
+        costEl.textContent = `Cost: ${cost}`;
+        shopItem.appendChild(costEl);
+
+        let amtEl = document.createElement('div');
+        amtEl.id = `shopItem-${item.name}-amt`;
+        amtEl.textContent = `Amount: ${item.amount}`;
+        shopItem.appendChild(amtEl);
+                                        
+        shopEl.appendChild(shopItem);
+
+        shopItem.addEventListener('click', () => {
+            if(item.amount + 1 > item.maxCount) {
+                createNotification(`Maximum amount purchased ${item.maxCount}`);
+            } else {
+                let cost = item.amount == 0 ? item.cost : Math.round(item.cost * item.amount * 22);
+                if(buyItem(cost)) {
+                    item.onClick();
+                    item.amount++;
+                }
+            }
+
+            updateUi();
+        });
+    });
+}
+
+function autoclicker() {
+    if(shopItems[1].amount > 0) {
+        let amt = shopItems[0].amount == 0 ? 1 : (shopItems[0].amount + 1) ** 2;
+        addPoints(amt * shopItems[1].amount);
     }
-    updateUi();
 }
-
-window.setInterval(function(){ 
-    autoClicker();
-    save();
-}, 10000);
 
 /* START */
 
-clickerEl.addEventListener('click', clicked);
+init();
 
-shopItems.forEach((shopItem) => {
-    shopItem.addEventListener('click', () => {
-        buyItem(shopItem.id);
-    }); 
+window.setInterval(() => {
+    autoclicker();
+    save();
+    document.title = `${numberWithCommas(points)} Points - Random Clicker`;
+    updateUi();
+}, 10000);
+
+clickerButtonEL.addEventListener('click', (e) => {
+    clicked(e);
 });
 
-init();
+saveBtn.addEventListener('click', save);
